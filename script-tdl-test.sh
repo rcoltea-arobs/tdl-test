06b36880270f05e6321bf6f6848d69ca81bc7b81
root <root@ip-172-31-37-86.eu-west-1.compute.internal>
2021-07-07 08:05:06 +0000

    updated index.html - crond

--- script-tdl-test.sh
+++ script-tdl-test.sh
@@ -3,6 +3,8 @@ wget -O /var/www/html/tdl-test/index.html https://www.irelandwestairport.com/fli
 
 sed -i -e 's;img src="/;img src="https://www.irelandwestairport.com/;g' /var/www/html/tdl-test/index.html 
 
+cd /var/www/html/tdl-test/
+
 git add .
 
 git commit -m "updated index.html - crond"
